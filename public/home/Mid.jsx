45ff08fcb34cbb2fdb0a2166ba704a3934115b3a
priyanshudangi011 <priyanshudangi011@gmail.com>
2025-11-20 19:13:54 +0530

    Updated project

--- public/home/Mid.jsx
+++ public/home/Mid.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import './btn.css';
 
 const Mid = () => {
   return (
@@ -6,7 +7,7 @@ const Mid = () => {
         <div className='upper text-left  text-[50px] font-[600] leading-[1]'><h1>Streaming Anime content</h1>
         <h1>quality content is here </h1>
         <h1>enjoy it</h1></div>
-        <button className='p-[5px] px-[50px] pb-[10px] text-[20px] font-[500]  bg-gradient-to-r from-orange-100 to-blue-100 border-none mt-[20px] ml-[10px] rounded-full  active:scale-[0.95] duration-300 ease-in' ><a href="http://localhost:5173/animelist">Explore</a></button>
+        <button  className='btn p-[5px] px-[50px] pb-[10px] text-[20px] font-[500]  bg-gradient-to-r from-orange-100 to-blue-100 border-none mt-[20px] ml-[10px] rounded-full  active:scale-[0.95] duration-300 ease-in' ><a href="http://localhost:5173/animelist">Explore</a></button>
          {/* <a href={href} className="inline-block">
       <button className="
         relative overflow-hidden
